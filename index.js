25155e087d1750df794d10f9413ecd0f37a45cc0
Omar Alexander Salazar de León <osalazar-2017359@kinal.edu.gt>
2022-07-15 10:48:13 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var user = require('./controllers/user.controller')
 var port = 3800;
-
+//45.171.65.193
 mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.DATABASE_CONNECT , {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>{
+mongoose.connect("mongodb+srv://osalazar-2017359:dwhdNFXz@cluster0.ybev4.mongodb.net/?retryWrites=true&w=majority"
+    //process.env.DATABASE_CONNECT
+    , { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
         user.initAdmin();
         console.log('connect to DB');
-        app.listen(process.env.PORT || port, ()=>{
+        app.listen(process.env.PORT || port, () => {
             console.log('server express running')
         })
-    }).catch((err)=>console.log('connection error to DB', err))
+    }).catch((err) => console.log('connection error to DB', err))
 
-    
